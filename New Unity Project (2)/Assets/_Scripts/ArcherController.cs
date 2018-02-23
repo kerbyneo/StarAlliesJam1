085ce85b293e829102435b38e47f96bc9cb30f9b
Eric <ebuckland18@punahou.edu>
2018-02-23 08:29:28 -1000

    Added Archer Count

---
+++ New Unity Project (2)/Assets/_Scripts/ArcherController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherController : MonoBehaviour {
+
+	public bool touchedGround = false;
+	
+	// check if touched something under world
+	void OnCollision(Collision collision) {
+		if (collision.gameObject.transform.root.gameObject.name == "World") {
+		
+			this.touchedGround = true;
+		
+		}
+	}
+}
---
+++ New Unity Project (2)/Assets/_Scripts/Archers_CountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private Text textF;
+
+	// Use this for initialization
+	void Start () {
+		textF = this.GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int archerCount = 0;
+
+		if (!GameObject.Find ("Archer").GetComponent<ArcherController> ().touchedGround) {
+		
+			archerCount++;
+
+		}
+
+		if (GameObject.Find ("Archer (1)").GetComponent<ArcherController> ().touchedGround) {
+
+			archerCount++;
+
+		}
+
+		if (GameObject.Find ("Archer (2)").GetComponent<ArcherController> ().touchedGround) {
+
+			archerCount++;
+
+
+		}
+
+
+		textF.text = archerCount.ToString ();
+
+	}
+}
