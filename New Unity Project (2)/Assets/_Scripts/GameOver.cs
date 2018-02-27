5d23c3ef1acc4466e82c32108adba737a00446f2
Shandy Kim <shandyk18@lakesideschool.org>
2018-02-26 22:45:19 -0800

    Added in a game over script that displays "Game Over" if player or all three archers fall off

---
+++ New Unity Project (2)/Assets/_Scripts/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+    private Rigidbody ballRB;
+    public Text gameOverText;
+    public GameObject archers;
+    public Archers_CountController archerScript;
+
+	// Use this for initialization
+	void Start () {
+        ballRB = this.GetComponent<Rigidbody>();
+        archers = GameObject.Find("Archers_Count");
+        archerScript = archers.GetComponent<Archers_CountController>(); //accessing script
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Water") //if tower or sphere collides with water
+        {
+            //Debug.Log("Water");
+            gameOverText.text = "Game Over";
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if(archerScript.archerCount == 0) //if all archers fall off
+        {
+            gameOverText.text = "Game Over";
+        }
+	}
+}
