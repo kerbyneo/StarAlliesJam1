18bacf64850c9bb2983d3e66d8d90261d157ee2a
Eric <ebuckland18@punahou.edu>
2018-03-02 11:09:24 -1000

    Started on checkpoint script, added checkpoint collider

---
+++ New Unity Project (2)/Assets/Models/flag/CheckPointController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour {
+
+
+	public GameObject flag1;
+	public GameObject flag2;
+
+	private GameObject checkpoint;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		flag1 = this.gameObject.GetComponent<Material> ();
+		flag2 = this.gameObject.GetComponent<Material> ();
+
+		checkpoint = this.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+	}
+
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.name == "Sphere") {
+		
+		
+		
+		
+		
+		
+		}
+	}
+
+}
